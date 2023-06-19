c14a92b9da52a8589ef501d70bfe29491da1b021
Oscar Alva <oscaraldrin23@gmail.com>
2023-06-19 21:39:57 +0200

    hasta aqui funciona el menu ngmodule

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-
-// el objetivo de este modulo es exportar todos los modulos que voy a necesitar de primeNg
 import { MenuModule } from 'primeng/menu';
 
+
 @NgModule({
-  declarations: [],
-  exports: [
+
+  exports : [
     MenuModule
   ]
 })
